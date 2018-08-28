0c4c1a024c91760120266fa2e9c106e1f16d041b
Sol Kim <solkim18@gmail.com>
2018-08-28 13:25:44 -0500

    cleaned up doc; wrote and tested removeFirst() in SinglyLinkedList

--- Data Structures/SinglyLinkedList.java
+++ Data Structures/SinglyLinkedList.java
@@ -18,7 +18,7 @@ public class SinglyLinkedList {
 		SinglyLinkedList list = new SinglyLinkedList();
 		
 		//tested addFirst and display
-		
+		/**
 		list.addFirst(new Node(20));
 		list.display();
 		list.addFirst(new Node(10));
@@ -33,16 +33,28 @@ public class SinglyLinkedList {
 		list.display();
 		list.addLast(new Node(2));
 		list.display();
+		*/
 		list.addLast(new Node(3));
 		list.display();
+		
+		
+		//tested addLast and display
+		
+		list.removeFirst();
+		list.display();
+		list.removeFirst();
+		list.display();
+		list.removeFirst();
+		list.display();
+	
 		
 	}
 	
 	/**
 	* Insert node to the beginning of the linked list: O(1)
 	*
-	* @param
-	* @return
+	* @param firstNode the node to add to the beginning of the list
+	* @return none
 	*/
 	public void addFirst(Node firstNode) {
 		if (head==null && tail==null) { //if the linked list is empty
@@ -60,8 +72,8 @@ public class SinglyLinkedList {
 	/**
 	* Add node to the end of the linked list: O(n)
 	*
-	* @param
-	* @return
+	* @param lastNode the node to add to the end of the linked list
+	* @return none
 	*/
 	public void addLast(Node lastNode) {
 		if (head==null && tail==null) { //if the linked list is empty
@@ -77,26 +89,57 @@ public class SinglyLinkedList {
 	}
 	
 	/**
-	* Delete the first node of the linked list: O(1)
+	* Remove the first node of the linked list: O(1)
 	*
-	* @param
-	* @return
+	* @param none
+	* @return the first node of the linked list removed
 	*/
-	public void removeFirst(Node endNode) {
+	public Node removeFirst() {
+		Node firstNode = null;
+		
+		/**
 		if (head==null && tail==null) { //if empty
 			System.out.println("Linked list is empty! Can't remove!");
+			throw new NullPointerException();
+		} else if (head == tail) { //if only one element in the linked list
+			head = tail = null;
 		} else {
-			
+			firstNode = head;
+			head = head.next;
+			firstNode.next = null;
+			tail.next = head;
+		}
+		*/
+		
+		try {
+			if (head==null && tail==null) { //if empty
+				System.out.println("Linked list is empty! Can't remove!");
+				//System.out.println("test 1");
+			} else if (head.equals(tail)) { //if only one element in the linked list
+				head = tail = null;
+				//System.out.println("test 2");
+			} else {
+				//System.out.println("test 3");
+				firstNode = head;
+				head = head.next;
+				firstNode.next = null;
+				tail.next = head;
+			}
+		} catch (NullPointerException npEx) {
+			//System.out.println("test 4");
+			npEx.printStackTrace();
+		} finally {
+			return firstNode;
 		}
 	}
 	
 	/**
-	* Delete the last node of the linked list: O(n)
+	* Remove the last node of the linked list: O(n)
 	*
 	* @param
 	* @return
 	*/
-	public void removeLast(Node endNode) {
+	public void removeLast() {
 		//if empty
 		//if the only node
 	}
@@ -104,8 +147,8 @@ public class SinglyLinkedList {
 	/**
 	* Returns the size of the linked list: O(n)
 	*
-	* @param
-	* @return
+	* @param none
+	* @return the size of the linked list
 	*/
 	public int size() {
 		//while linked list node does not equal the head
@@ -115,37 +158,44 @@ public class SinglyLinkedList {
 	/**
 	* Returns the node at given index; if index does not exist prints "INDEX DOES NOT EXIST" and returns a null node: O(n)
 	*
-	* @param
-	* @return
+	* @param index the index of the node to be returned
+	* @return the node at the given index
 	*/
-	public Node getNode() {
+	public Node getNode(int index) {
 		return null; //dummy node
 	}
 	
 	/**
 	* Adds a node to the given index. If index does not exist prints "INDEX DOES NOT EXIST": O(n)
 	*
-	* @param
-	* @return
+	* @param index the index of the node to be added
+	* @return none
 	*/
-	public void addNode(Node endNode) {
+	public void addNode(int index) {
 		
 	}
 	
 	/**
 	* Prints the entire linked list: O(n)
 	*
-	* @param
-	* @return
+	* @param none
+	* @return none
+	* @throws NullPointerException
 	*/
 	public void display() {
+		//System.out.println("i'm in display");
 		Node printNode = head;
-		while(printNode != tail) {
-			System.out.print(printNode.data + ", ");
-			printNode = printNode.next;
+		
+		if (printNode != null) { //only display if the linked list is not empty
+			while(printNode != tail) {
+				System.out.print(printNode.data + ", ");
+				printNode = printNode.next;
+			}
+			System.out.println(printNode.data); //last node
+			System.out.println(" head is " + head.data + ", tail is " + tail.data);
+		} else {
+			System.out.println("No nodes to display :/");
 		}
-		System.out.println(printNode.data); //last node
-		System.out.println(" head is " + head.data + ", tail is " + tail.data);
 	}
 }
 
